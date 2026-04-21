2191dc3c6890e0e4ad0c53f05613644e93b4e308
Juraj Zec <juro.zec.1@gmail.com>
2026-04-21 17:28:12 +0200

    seeded dummy completedExercise records into the database

--- src/seed.ts
+++ src/seed.ts
@@ -2,7 +2,7 @@ import { models, sequelize } from '~/db'
 import { EXERCISE_DIFFICULTY, USER_ROLE } from '~/utils/enums'
 import bcrypt from 'bcrypt'
 
-const { Exercise, Program, User } = models
+const { CompletedExercise, Exercise, Program, User } = models
 
 const seedDB = async () => {
   await sequelize.sync({ force: true })
@@ -73,6 +73,30 @@ const seedDB = async () => {
       email: 'mary@a.co',
       password: pass,
       role: USER_ROLE.USER,
+    },
+  ])
+
+  await CompletedExercise.bulkCreate([
+    {
+      userID: 2,
+      exerciseID: 1,
+      startedAt: new Date('2026-04-18T08:25:00Z'),
+      completedAt: new Date('2026-04-18T08:30:00Z'),
+      durationSeconds: 300,
+    },
+    {
+      userID: 2,
+      exerciseID: 3,
+      startedAt: new Date('2026-04-19T17:01:00Z'),
+      completedAt: new Date('2026-04-19T17:10:00Z'),
+      durationSeconds: 540,
+    },
+    {
+      userID: 3,
+      exerciseID: 2,
+      startedAt: new Date('2026-04-20T11:53:00Z'),
+      completedAt: new Date('2026-04-20T12:00:00Z'),
+      durationSeconds: 420,
     },
   ])
 }
